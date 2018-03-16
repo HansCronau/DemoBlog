d8e388e92b64bd4d8fdcfd503288f9f408f5e268
HansCronau <hans@cronau.nl>
2018-03-16 22:58:58 +0100

    Adds basic pagination functionality.

--- src/Blog.js
+++ src/Blog.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import BlogArticle from './BlogArticle.js';
 import BlogSearch from './BlogSearch.js';
-import { Container, Grid, Segment, Transition, Loader } from 'semantic-ui-react'
+import { Container, Grid, Pagination, Segment, Transition, Loader } from 'semantic-ui-react'
 
 // For demoing/testing with more than one article.
 const fakeArticles = _.times(10, () => ({
@@ -15,13 +15,24 @@ const fakeArticles = _.times(10, () => ({
     body: faker.lorem.paragraphs(3),
 }))
 
+const Centered = ({children}) =>
+    <Grid columns='equal'>
+        <Grid.Column />
+        <Grid.Column>
+            {children}
+        </Grid.Column>
+        <Grid.Column />
+    </Grid>
+
 class Blog extends Component {
+    
     constructor() {
         super();
         this.state = {
             articles: [],
             isLoading: true,
-            pageNumber: 1,
+            activePage: 1,
+            articlesPerPage: 5,
         };
     }
 
@@ -45,18 +56,42 @@ class Blog extends Component {
         })
     }
 
+    handlePaginationChange = (e, {activePage}) => this.setState({activePage});
+
+    pagination() {
+        const {articles, isLoading, activePage, articlesPerPage} = this.state;
+        return isLoading ? null : <Pagination
+            totalPages={_.ceil(articles.length/articlesPerPage)}
+            activePage={activePage}
+            siblingsRange={2}
+            firstItem={null}
+            lastItem={null}
+            onPageChange={this.handlePaginationChange}
+        />
+    }
+
     render() {
+        const {articles, isLoading, activePage, articlesPerPage} = this.state;
+        const firstArticle = (activePage-1)*articlesPerPage;
+        const lastArticle = firstArticle + articlesPerPage;
         return (
             <Segment basic>
-                <Grid stackable columns={2}>
+                <Grid stackable stretched columns={2}>
                     <Grid.Column width={4}>
-                        <BlogSearch articlesData={this.state.articles} />
+                        <BlogSearch articlesData={articles} />
                     </Grid.Column>
                     <Grid.Column width={12}>
-                        {this.state.articles.map( article =>
-                            <BlogArticle key={article.id} articleData={article} />
-                        )}
-                        <Loader active={this.state.isLoading} />
+                        {
+                            articles.slice(firstArticle, lastArticle)
+                            .map( article => <BlogArticle
+                                key={article.id}
+                                articleData={article}
+                            />)
+                        }
+                        <Centered>
+                            {this.pagination()}
+                        </Centered>
+                        <Loader active={isLoading} />
                     </Grid.Column>
                 </Grid>
             </Segment>
